c8461fdb6d288c476eddc42d06f3d9788d1927a1
Shark. <geneuto@gmail.com>
2025-11-26 14:49:57 -0300

    Change color button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -158,7 +158,7 @@ const Hero = () => {
               id="watch-trailer"
               title="Create Companion"
               leftIcon={<TiLocationArrow />}
-              containerClass="bg-yellow-300 flex-center gap-1"
+              containerClass="bg-yellow-500 flex-center gap-1"
             />
           </div>
         </div>
